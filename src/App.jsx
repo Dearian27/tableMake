c0eaa53fd3bab24c38a439bfe617cb21338fd905
Dearian <dearian2706@gmail.com>
2023-07-18 00:25:34 +0300

    model in scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-
+import { Stage, Center, AccumulativeShadows, RandomizedLight, Environment, OrbitControls } from '@react-three/drei'
+// import { useControls, button } from 'leva'
 function App() {
   return (
     <>
-      <Canvas>
-        <Experience />
-      </Canvas>
+    {/* <Canvas shadows>
+      <Stage 
+      // shadows="accumulative"
+      >
+        <mesh castShadows position={[-1.5, 0, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+        </mesh>
+      </Stage>
+    </Canvas> */}
+    <Canvas 
+    // gl={{ antialias: false, preserveDrawingBuffer: true }}
+      shadows
+      camera={{ position: [4, 4, -12], fov: 35 }}
+     >
+      <group position={[0, -0.75, 0]}>
+        <Center top> 
+          <Experience />
+        </Center>
+        {/* // <AccumulativeShadows>
+        //  <RandomizedLight position={[2, 5, 5]} />
+        // </AccumulativeShadows>  */}
+      </group>
+     {/* // <OrbitControls makeDefault minPolarAngle={0} maxPolarAngle={Math.PI / 2} />
+      // <Environment preset="dawn" background blur={1} />  */}
+    </Canvas>
+    
     </>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,13 +1,21 @@
 import { OrbitControls } from "@react-three/drei";
-
+import { Table } from "./Table";
 export const Experience = () => {
   return (
     <>
-      <OrbitControls />
-      <mesh>
-        <meshNormalMaterial />
-        <boxBufferGeometry />
-      </mesh>
+      {/* <Stage 
+        intensity={1.5}
+        environment="city"
+        shadows={{type: 'accunulative', color: "d9afd9", colorBlend: 2, opacity: 2}}
+        adjustCamera={0.9}
+      > */}
+        <Table />
+      {/* </Stage> */}
+        <OrbitControls 
+        makeDefault
+        // minPolarAngle={Math.PI / 2}
+        maxPolarAngle={Math.PI / 2}
+        />
     </>
   );
 };
